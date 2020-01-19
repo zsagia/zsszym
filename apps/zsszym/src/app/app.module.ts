59f127b5882584fc648cb3b6882223e6345cf69c
zsagia <zsagia@gmail.com>
2020-01-19 12:25:48 +0100

    Add ng-zorro-antd as a ui library

--- apps/zsszym/src/app/app.module.ts
+++ apps/zsszym/src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule],
-    providers: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        NgZorroAntdModule,
+        FormsModule,
+        BrowserAnimationsModule
+    ],
+    providers: [{ provide: NZ_I18N, useValue: en_US }],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
